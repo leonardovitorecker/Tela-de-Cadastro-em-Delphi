379722135f44b938fbc0cf98ba16f0e877d1f3fa
leonardo ecker <leonardovitoecker@gmail.com>
2022-08-17 15:45:07 -0300

    Primeiro commit

---
+++ Unit2.pas
@@ -0,0 +1,31 @@
+unit Unit2;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Layouts, FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm2 = class(TForm)
+    ToolBar1: TToolBar;
+    Layout1: TLayout;
+    V: TLayout;
+    Image1: TImage;
+    Image2: TImage;
+    Label4: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.fmx}
+
+end.
